baf3db027e3ff15c0af3103e411dcf27ebf80b3d
KaydenLee03 <03kayden.lee@kamsc.org>
2025-09-25 13:36:20 -0400

    update

--- src/main/java/DroneAssignment.java
+++ src/main/java/DroneAssignment.java
@@ -5,22 +5,21 @@ public class DroneAssignment {
         double altitude = 150.7;
         double battery = 87.3;
         int photos = 0;
-        int displayAltitude = (int) altitude;
-        int batteryRounded = (int) battery;
 
-        
         System.out.println("=== Drone Flight Simulation ===");
         System.out.println("Initial Status:");
         System.out.println("Altitude: " + altitude + " meters");
         System.out.println("Battery: " + battery + "%");
         System.out.println("Photos: " + photos);
         System.out.println();
-        //System.out.println(displayAltitude);
-        //System.out.println(batteryRounded);
+
+        int displayAltitude = (int) altitude;
+        int batteryRounded = (int) battery;
+
         altitude += 20;
-        battery *= 0.85;
+        battery -= 15;
         photos += 3;
-        altitude /= 2;
+        altitude *= 0.5;
         photos %= 4;
 
         System.out.println("Photos: " + photos);
