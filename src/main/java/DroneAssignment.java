546a74f43742be61f9211f20c2b39993285846d1
KaydenLee03 <03kayden.lee@kamsc.org>
2025-09-25 13:32:08 -0400

    Debug and drone

--- src/main/java/DroneAssignment.java
+++ src/main/java/DroneAssignment.java
@@ -1,10 +1,13 @@
-// DO NOT CHANGE THE CLASS NAME, IT WILL BREAK THE AUTO GRADER
+
 public class DroneAssignment {
     public static void main(String[] args) {
         // Initial drone values
         double altitude = 150.7;
         double battery = 87.3;
         int photos = 0;
+        int displayAltitude = (int) altitude;
+        int batteryRounded = (int) battery;
+
         
         System.out.println("=== Drone Flight Simulation ===");
         System.out.println("Initial Status:");
@@ -12,19 +15,15 @@ public class DroneAssignment {
         System.out.println("Battery: " + battery + "%");
         System.out.println("Photos: " + photos);
         System.out.println();
-        
-        // TODO: Part 1 - Casting
-        // Cast altitude (double) to int called displayAltitude
-        // Cast battery (double) to int called batteryRounded
-        // Print them to check your work (optional)
-        
-        // TODO: Part 2 - Compound Assignment Operators
-        // Altitude: Drone climbs 20 meters 
-        // Battery: Battery drains 15% 
-        // Photos: Drone takes 3 photos 
-        // Altitude: Drone descends to half its current altitude 
-        // use modulo to calculate storage slots used on photos to calculate storage slots used
-        // Display "Photos: " + photos
-        // Display "Storage slots used: " + photos + " out of 4"
+        //System.out.println(displayAltitude);
+        //System.out.println(batteryRounded);
+        altitude += 20;
+        battery *= 0.85;
+        photos += 3;
+        altitude /= 2;
+        photos %= 4;
+
+        System.out.println("Photos: " + photos);
+        System.out.println("Storage slots used: " + photos + " out of 4");
     }
 }
